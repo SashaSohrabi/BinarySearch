b93cf227bc9b2d25eda63b097240c83f5a420792
ShahabSohrabi <shb.sohrabi@gmail.com>
2017-07-26 14:01:28 +0300

    Add files via upload
    modified

--- src/test/BinarySearchTest.java
+++ src/test/BinarySearchTest.java
@@ -4,39 +4,40 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class BinarySearchTest {
 
-	int search = 3;
-	private BinarySearch binary_search;
+    int search = 3;
+    private BinarySearch binary_search;
 
-	@Before
-	public void setup() {
-		System.out.println("Setup");
-		binary_search = new BinarySearch();
-	}
+    @Before
+    public void setup() {
+        System.out.println("Setup");
+        binary_search = new BinarySearch();
+    }
 
-	@Test
-	public void binarySearchInSortedArray() {
-		System.out.println("Binary search in sorted array");
-		int[] array = new int[]{1, 3, 4, 5, 6, 9, 10, 15};
-		boolean result = binary_search.binarySearch(array, search);
-		assertTrue(result);
-	}
+    @Test
+    public void binarySearchInSortedArray() {
+        System.out.println("Binary search in sorted array");
+        int[] array = new int[]{1, 3, 4, 5, 6, 9, 10, 15};
+        int result = binary_search.binarySearch(array, search);
+       assertEquals(search, result);
+    }
 
-	@Test
-	public void binarySearchInNonSortedArray() {
-		System.out.println("Binary search in non sorted array");
-		int[] array = new int[]{5, 6, 9, 10, 15, 1, 3, 4};
-		boolean result = binary_search.binarySearch(array, search);
-		assertTrue(result);
-	}
+    @Test
+    public void binarySearchInNonSortedArray() {
+        System.out.println("Binary search in non sorted array");
+        int[] array = new int[]{5, 6, 9, 10, 15, 1, 3, 4};
+        int result = binary_search.binarySearch(array, search);
+        assertEquals(search, result);
+    }
 
-	@After
-	public void teardown() {
-		System.out.println("Teardown");
-		search = 0;
-	}
+    @After
+    public void teardown() {
+        System.out.println("Teardown");
+        search = 0;
+    }
 
 }
